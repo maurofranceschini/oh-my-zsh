7a5f92562a1e570043143f6a75494695a8f810d4
Mauro Franceschini <mauro.franceschini@cedac.com>
2014-07-25 11:19:15 +0200

    Added the completion for jgitflow maven plugin.

--- plugins/mvn/mvn.plugin.zsh
+++ plugins/mvn/mvn.plugin.zsh
@@ -1,6 +1,6 @@
 function listMavenCompletions { 
 	 reply=(
-		cli:execute cli:execute-phase archetype:generate generate-sources compile clean install test test-compile deploy package cobertura:cobertura jetty:run gwt:run gwt:debug -DskipTests -Dmaven.test.skip=true -DarchetypeCatalog=http://tapestry.formos.com/maven-snapshot-repository -Dtest= `if [ -d ./src ] ; then find ./src -type f | grep -v svn | sed 's?.*/\([^/]*\)\..*?-Dtest=\1?' ; fi`); 
+		cli:execute cli:execute-phase archetype:generate generate-sources compile clean install test test-compile deploy package cobertura:cobertura jetty:run gwt:run gwt:debug jgitflow:feature-start jgitflow:feature-finish jgitflow:release-start jgitflow:release-finish jgitflow:hotfix-start jgitflow:hotfix-finish jgitflow:build-number -DskipTests -Dmaven.test.skip=true -DarchetypeCatalog=http://tapestry.formos.com/maven-snapshot-repository -Dtest= `if [ -d ./src ] ; then find ./src -type f | grep -v svn | sed 's?.*/\([^/]*\)\..*?-Dtest=\1?' ; fi`); 
 }
 
 compctl -K listMavenCompletions mvn
